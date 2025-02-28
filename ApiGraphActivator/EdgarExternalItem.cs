c0d328b2be66ed68fbc3f7ec020a00be77116637
Brian <brian.m.odonnell@gmail.com>
2025-02-28 13:50:05 -0600

    removal of secrets

---
+++ ApiGraphActivator/EdgarExternalItem.cs
@@ -0,0 +1,26 @@
+﻿namespace ApiGraphActivator
+{
+    public class EdgarExternalItem
+    {
+        public string? idField { get; set; }
+        public string? titleField { get; set; }
+        public string? companyField { get; set; }
+        public string? urlField { get; set; }
+        public string? reportDateField { get; set; }
+        public string? formField { get; set; }
+        public string? contentField { get; set; }
+
+        public EdgarExternalItem(string id, string title, string company, string url, string reportDate, string form, string content)
+        {
+            idField = id;
+            titleField = title;
+            companyField = company;
+            urlField = url;
+            reportDateField = reportDate;
+            formField = form;
+            contentField = content;
+        }
+    }
+
+
+}
---
+++ ApiGraphActivator/Services/ContentService.cs
@@ -0,0 +1,110 @@
+using ApiGraphActivator;
+using ApiGraphActivator.Services;
+using Markdig;
+using Microsoft.Graph.Models.ExternalConnectors;
+
+// Define a static class named ContentService
+static class ContentService
+{
+  // Define a static list to hold EdgarExternalItem objects
+  static List<EdgarExternalItem> content;
+
+  // Define a static list to hold ExternalItem objects
+  static List<ExternalItem> items = new();
+
+  // Define an asynchronous static method named Extract
+  async static Task Extract()
+  {
+    // Populate the content list with data from the EdgarService
+    content = await EdgarService.HydrateLookupData();
+  }
+
+  // Define a static method named Transform that takes an EdgarExternalItem as a parameter
+  static void Transform(EdgarExternalItem item)
+  {
+    // Create a new ExternalItem object and populate its properties
+    ExternalItem exItem = new ExternalItem
+    {
+      Id = EdgarService.itemId, // Set the Id property using the EdgarService itemId
+      Properties = new()
+      {
+        // Set the AdditionalData dictionary with various properties from the EdgarExternalItem
+        // This applies to the schema that was defined in the ConnectionService
+        AdditionalData = new Dictionary<string, object> {
+            { "Title", item.titleField },
+            { "Company", item.companyField },
+            { "Url", item.urlField },
+            { "DateFiled", item.reportDateField },
+            { "Form", item.formField }
+          }
+      },
+      Content = new()
+      {
+        // Set the content and type of the ExternalItem
+        Value = item.contentField,
+        Type = ExternalItemContentType.Html
+      },
+      Acl = new()
+        {
+          // Set the Access Control List (ACL) for the ExternalItem
+          new()
+          {
+            Type = AclType.Group,
+            Value = "EdgarDataUsers",
+            AccessType = AccessType.Grant
+          }
+        }
+    };
+
+    // Add the created ExternalItem to the items list
+    items.Add(exItem);
+  }
+
+  // Define an asynchronous static method named Load
+  static async Task Load()
+  {
+    // Iterate over each item in the items list
+    foreach (var item in items)
+    {
+      // Output a message to the console indicating the start of the item loading process
+      Console.Write(string.Format("Loading item {0}...", item.Id));
+      try
+      {
+        // Await the asynchronous operation of putting the item into the GraphService client
+        await GraphService.Client.External
+          .Connections[Uri.EscapeDataString(ConnectionConfiguration.ExternalConnection.Id!)]
+          .Items[item.Id]
+          .PutAsync(item);
+        // Output a message to the console indicating the completion of the item loading process
+        Console.WriteLine("DONE");
+
+        // Get the URL from the item's AdditionalData dictionary
+        string url = (string)item.AdditionalData["Url"];
+        // Await the asynchronous operation of updating the processed item in the EdgarService
+        await EdgarService.UpdateProcessedItem(url);
+      }
+      catch (Exception ex)
+      {
+        // Output an error message to the console if an exception occurs
+        Console.WriteLine("ERROR");
+        Console.WriteLine(ex.Message);
+      }
+    }
+  }
+
+  // Define a public asynchronous static method named LoadContent
+  public static async Task LoadContent()
+  {
+    // Call the Extract method to populate the content list
+    await Extract();
+
+    // Iterate over each item in the content list and transform it
+    foreach (var item in content)
+    {
+      Transform(item);
+    }
+
+    // Call the Load method to load the transformed items
+    await Load();
+  }
+}
