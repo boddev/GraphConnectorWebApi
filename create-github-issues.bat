e550d7d1fc6cbf74c8d10a227fbafc195eef0770
Brian ODonnell <bodonnell@microsoft.com>
2025-07-31 11:11:01 -0500

    removing unused files

--- create-github-issues.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Creating GitHub Issues and Milestones...
-echo.
-
-pwsh -NoProfile -ExecutionPolicy Bypass -File "create-github-issues.ps1"
-
-echo.
-echo Done! Check your GitHub repository for the created issues.
-pause
